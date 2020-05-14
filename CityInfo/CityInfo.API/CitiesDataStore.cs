656953d226d79a623ec5674af3c7c8402a7853d4
Fernando Feliu Goncalez <ffeliu2@gmail.com>
2020-05-14 08:56:20 -0300

    Project Creation

---
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,81 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDTO> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDTO>()
+            {
+               new CityDTO()
+               {
+                   Id = 1,
+                   Name = "Brasilia",
+                   Description = "bla bla bla",
+                   PointsOfInterest = new List<PointsOfInterestDTO>()
+                   {
+                       new PointsOfInterestDTO()
+                       {
+                           Id = 1,
+                           Name = "Ponto 1"
+                       },
+                        new PointsOfInterestDTO()
+                       {
+                           Id = 2,
+                           Name = "Ponto 2"
+                       }
+                   }
+               },
+               new CityDTO()
+               {
+                   Id = 2,
+                   Name = "São Paulo",
+                   Description = "ble ble ble",
+                   PointsOfInterest = new List<PointsOfInterestDTO>()
+                   {
+                       new PointsOfInterestDTO()
+                       {
+                           Id = 3,
+                           Name = "Ponto 3"
+                       },
+                        new PointsOfInterestDTO()
+                       {
+                           Id = 4,
+                           Name = "Ponto 4"
+                       }
+                   }
+               },
+               new CityDTO()
+               {
+                   Id = 3,
+                   Name = "Porto",
+                   Description = "bli bli bli",
+                   PointsOfInterest = new List<PointsOfInterestDTO>()
+                   {
+                       new PointsOfInterestDTO()
+                       {
+                           Id = 5,
+                           Name = "Ponto 5"
+                       },
+                        new PointsOfInterestDTO()
+                       {
+                           Id = 6,
+                           Name = "Ponto 6"
+                       }
+                   }
+               }
+            };
+        }
+
+
+    }
+}
---
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityid}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPointsOfInterest(int cityId) 
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+                return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+                return NotFound();
+
+            var point = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (point == null)
+                return NotFound();
+
+            return Ok(point);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId, 
+            [FromBody] PointsOfInterestForCreationDTO pointsOfInterestForCreationDTO)
+        {
+            validateModel(pointsOfInterestForCreationDTO);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+                return NotFound("City not Found");
+
+            var maxPointOfInterestedID = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointsOfInterestDTO()
+            {
+                Id = ++maxPointOfInterestedID,
+                Name = pointsOfInterestForCreationDTO.Name,
+                Description = pointsOfInterestForCreationDTO.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", 
+                new { cityId, id = finalPointOfInterest.Id},
+                finalPointOfInterest);
+        }
+
+        private void validateModel(PointsOfInterestForCreationDTO pointsOfInterestForCreationDTO)
+        {
+            if (String.IsNullOrEmpty(pointsOfInterestForCreationDTO.Name))
+            {
+                ModelState.AddModelError(
+                    "Nome",
+                    "Nome inválido");
+
+                return;
+            }
+
+            if (String.IsNullOrEmpty(pointsOfInterestForCreationDTO.Description))
+            {
+                ModelState.AddModelError(
+                    "Descrição",
+                    "Descrição inválida");
+
+                return;
+            }
+
+            if (pointsOfInterestForCreationDTO.Name.Length > 50)
+            {
+                ModelState.AddModelError(
+                    "Tamanho do campo nome",
+                    "O campo nome não pode ser maior que 50 caracteres");
+            }
+
+            if (pointsOfInterestForCreationDTO.Description.Length > 255)
+            {
+                ModelState.AddModelError(
+                    "Tamanho do campo descrição",
+                    "O campo descrição não pode ser maior que 255 caracteres");
+            }
+        }
+    }
+}
